6bc2a2c54a59ae895012b44454f2f099ff344542
gdsakelaris <gdsakelaris@gmail.com>
2023-07-24 21:37:15 -0500

    idk4

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # README
 
-## idk3
+## idk4
---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// const path = require('path');
+
+module.exports = function override(config) {
+    // Add fallback for 'path' module
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        path: require.resolve('path-browserify'),
+    };
+    return config;
+};
